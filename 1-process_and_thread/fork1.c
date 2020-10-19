ccdfe9ec3a52315acc439408c810bca7b880116e
Lincyaw <814750204@qq.com>
2020-10-19 14:21:20 +0800

    delete bin files

--- 1-process_and_thread/fork1.c
+++ 1-process_and_thread/fork1.c
@@ -2,21 +2,23 @@
 
 /* $begin fork */
 /* $begin wasidefork */
-int main() 
+int main()
 {
     pid_t pid;
     int x = 1;
 
     pid = Fork(); //line:ecf:forkreturn
-    if (pid == 0) {  /* Child */
-	printf("child : x=%d\n", ++x); //line:ecf:childprint
-	exit(0);
+    if (pid == 0)
+    {                                  /* Child */
+        printf("child : x=%d\n", ++x); //line:ecf:childprint
+        //exit(0);
+    }else{
+        printf("not children: %d\n",pid);
     }
 
     /* Parent */
-    printf("parent: x=%d\n", --x); //line:ecf:parentprint
+    printf("parent: x=%d\n", x-2); //line:ecf:parentprint
     exit(0);
 }
 /* $end fork */
 /* $end wasidefork */
-
